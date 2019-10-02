aa1eb47e090245007caa8b3c05f7b3a65f2f661b
Angelica Zamer <angelica.zamer@gmail.com>
2019-10-02 14:52:23 -0500

    URL & axios used to output response.data

--- liri.js
+++ liri.js
@@ -1,4 +1,18 @@
 require("dotenv").config();
 var keys = require("./keys.js");
+var Spotify = require("node-spotify-api");
+var axios = require("axios");
 
 var spotify = new Spotify(keys.spotify);
+
+
+
+var concertThis = function(band){
+    var url = "https://rest.bandsintown.com/artists/" + band + "/events?app_id=codingbootcamp"
+    axios.get(url).then(function(response){
+        debugger;
+        console.log(response.data[0].venue.name);
+    })
+}
+
+concertThis("tyler the creator");
