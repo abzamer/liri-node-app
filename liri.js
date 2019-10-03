2a7deaee9b93e30c4b98b9208aaac543138892f1
Angelica Zamer <angelica.zamer@gmail.com>
2019-10-02 22:54:45 -0500

    movie-this working when movie is hard-coded

--- liri.js
+++ liri.js
@@ -9,9 +9,36 @@ var spotify = new Spotify(keys.spotify);
 var concertThis = function(band){
     var url = "https://rest.bandsintown.com/artists/" + band + "/events?app_id=codingbootcamp"
     axios.get(url).then(function(response){
-        debugger;
+        // debugger;
+        //structure it & ensure path is correct
         console.log(response.data[0].venue.name);
+        console.log(response.data[0].venue.location);
+        console.log(response.data[0].venue.name)
     })
 }
 
-concertThis("lizzo");
+// concertThis("lizzo");
+
+//movie-this 
+var movieThis = function(movie){
+    var url = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy"
+    axios.get(url).then(function(response){
+        var movie = response.data;
+        console.log(
+            " Movie: " + movie.Title + "\n",
+            "Year: " + movie.Year + "\n",
+            "Rating: " + movie.Ratings[1].Value + "\n",
+            "Country: " + movie.Country + "\n",
+            "Language: " + movie.Language + "\n",
+            "Plot: " + movie.Plot + "\n",
+            "Actors: " + movie.Actors + "\n",
+            );
+    })
+}
+
+movieThis("Sharknado");
+
+
+//Spotify
+
+
