2a45869f0eb272c3815c9fde5edf8673f0a46aa3
Angelica Zamer <angelica.zamer@gmail.com>
2019-10-03 14:47:52 -0500

    movie-this is good to go; need default

--- liri.js
+++ liri.js
@@ -9,20 +9,22 @@ var spotify = new Spotify(keys.spotify);
 var concertThis = function(band){
     var url = "https://rest.bandsintown.com/artists/" + band + "/events?app_id=codingbootcamp"
     axios.get(url).then(function(response){
+        for(i=0; i < response.length; i++){
         // debugger;
         //structure it & ensure path is correct
         console.log(response.data[0].venue.name);
         console.log(response.data[0].venue.location);
         console.log(response.data[0].venue.name)
+        }
     })
 }
 
 // concertThis("lizzo");
 
 //movie-this 
-var movieThis = function(movie){
+var movieThis = function (movie) {
     var url = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy"
-    axios.get(url).then(function(response){
+    axios.get(url).then(function (response) {
         var movie = response.data;
         console.log(
             " Movie: " + movie.Title + "\n",
@@ -32,7 +34,7 @@ var movieThis = function(movie){
             "Language: " + movie.Language + "\n",
             "Plot: " + movie.Plot + "\n",
             "Actors: " + movie.Actors + "\n",
-            );
+        );
     })
 }
 
@@ -40,22 +42,33 @@ var movieThis = function(movie){
 
 
 //Spotify
+var spotifyThis = function(song){
+    spotify.search({type: "track", query: song}, 
+    function(err, data){
+        if (err){
+            return console.log("Error occurred" + err);
+        };
+        debugger;
+        console.log(data.tracks.items[0].artists[0].name);
+        console.log(data.tracks.items[0].name);
+        // console.log(data.tracks.items[0].preview_url);
+        console.log(`Album name: ${data.tracks.items[0].album.name}`)
+        //if no song provided, play Ace of Base's The Sign
+    });
+}
 
-var song = "All the small things"
-spotify.search({type: "track", query: song}, 
-function(err, data){
-    if (err){
-        return console.log("Error occurred" + err);
-    };
-    debugger;
-    console.log(data.tracks.items[0].artists[0].name);
-    //song, preview, album
-    console.log(data.tracks.items[0].name);
-    console.log(data.tracks.items[0].preview_url);
-    // console.log(data.tracks.items[0].artists[0].name);
-    //if no song provided, play Ace of Base's The Sign
-    
+var whichCommand = function(action,value){
 
-});
+    if(action==="concert-this"){
+        concertThis(value)
+    }else if(action==="spotify-this-song"){
+        spotifyThis(value);
+    } else if(action === "movie-this"){
+        movieThis(value);
+    }
+} 
 
+var userCommand = process.argv[2];
+var userValue = process.argv.splice(3).join("+")
 
+whichCommand(userCommand,userValue);
